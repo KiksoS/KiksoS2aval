4f1165945683acb79410eea6b3aba4175884975d
kikso <quiquevolc@gmail.com>
2023-02-16 09:11:42 +0100

    cmedia.sh hecho

---
+++ cmedia.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+linea=`cat ./consumo.txt | wc -l`
+
+total=0
+media=0
+for i in `seq 2 $linea`
+do
+nombre=`cat ./consumo.txt | head -n$i | tail -n1 | awk '{print $1}'`
+if [ $1 == $nombre ];
+    then
+        consumo=`cat ./consumo.txt | head -n$i | tail -n1 | awk '{print $4}'`
+        total=$((total+consumo))
+        ((media++))
+    fi
+done
+
+resultado=$((total/media))
+echo "La media de $1 es $resultado"
